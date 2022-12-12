82831c1ad2cac07160aa09a26635bbe0082b1d73
Hugh Nguyen <hhnguyen255@gmail.com>
2022-12-12 17:22:13 +0700

    setup basic editor form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { FC } from 'react';
+import { FC, ChangeEvent, MouseEvent, useState } from 'react';
 
 const App: FC = () => {
-  return <div>Jbook App</div>
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+          setInput(e.target.value)
+        }
+      ></textarea>
+      <button
+        onClick={(_e: MouseEvent<HTMLButtonElement>) => console.log(input)}
+      >
+        Submit
+      </button>
+      <pre>{code}</pre>
+    </div>
+  );
 };
 
 export default App;
