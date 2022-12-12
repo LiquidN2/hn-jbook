f1a3614b73eb29680e12594e8e27c34ab04167fb
Hugh Nguyen <hhnguyen255@gmail.com>
2022-12-12 17:37:52 +0700

    initialize esbuild in web browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { FC, ChangeEvent, MouseEvent, useState } from 'react';
+import { FC, ChangeEvent, MouseEvent, useEffect, useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
 
 const App: FC = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+
+  const startService = async () => {
+    try {
+      console.log('🕧 Starting Esbuild service...');
+      await esbuild.initialize({
+        worker: true,
+        wasmURL: 'https://unpkg.com/esbuild-wasm@0.16.4/esbuild.wasm',
+      });
+
+      console.log('✅ Esbuild started');
+      console.log(esbuild);
+    } catch (err: any) {
+      throw '💥 Unable to start Esbuild service';
+    }
+  };
+
+  useEffect(() => {
+    startService().catch((err: any) => console.error(err));
+  }, []);
 
   return (
     <div>
