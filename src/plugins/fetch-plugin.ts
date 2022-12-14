876db61a8e9b1c83d54fbf57c8c6a999969dd252
Hugh Nguyen <hhnguyen255@gmail.com>
2022-12-14 13:38:01 +0700

    added logic to load CSS module

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,14 +11,53 @@ export const fetchPlugin = (input: string) => ({
       contents: input,
     }));
 
-    // Fetch data for JS modules
+    // Fetch data locally if previously fetched
     build.onLoad({ filter: /.*/ }, async args => {
       try {
-        // Fetch from indexedDB if previously requested
-        const cachedOnLoadResult = await localforage.getItem(args.path);
-        if (cachedOnLoadResult)
-          return cachedOnLoadResult as esbuild.OnLoadResult;
+        const cachedResult = await localforage.getItem(args.path);
+        return cachedResult ? cachedResult : null;
+      } catch (err: any) {
+        throw err;
+      }
+    });
 
+    // Fetch CSS modules
+    build.onLoad({ filter: /.css$/ }, async args => {
+      try {
+        // Fetch from unpkg.com if not cached locally (indexedDB)
+        const { data, request } = await axios.get(args.path);
+
+        // Escape special chars (blank space, double quote & single quote)
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+        // Convert CSS to JS script which appends CSS to style element in the HTML doc head
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
+
+        const onLoadResult: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        // Save data to indexedDB (via localforage)
+        await localforage.setItem(args.path, onLoadResult);
+
+        return onLoadResult;
+      } catch (err: any) {
+        throw err;
+      }
+    });
+
+    // Fetch JS modules
+    build.onLoad({ filter: /.*/ }, async args => {
+      try {
         // Fetch from unpkg.com if not cached locally (indexedDB)
         const { data, request } = await axios.get(args.path);
         const onLoadResult: esbuild.OnLoadResult = {
@@ -33,7 +72,7 @@ export const fetchPlugin = (input: string) => ({
         // Return load result
         return onLoadResult;
       } catch (err: any) {
-        console.error('💥 Unable to fetch data', err);
+        throw err;
       }
     });
   },
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -4,20 +4,20 @@ export const unpkgPathPlugin = () => ({
   name: 'unpkg-path-plugin',
   setup(build: esbuild.PluginBuild) {
     // Resolving for dummy path 'index.js'
-    build.onResolve({ filter: /^index\.js$/ }, (args: any) => ({
+    build.onResolve({ filter: /^index\.js$/ }, args => ({
       path: args.path,
       namespace: 'a',
     }));
 
     // Resolving relative path './' and '../'
     // args.resolveDir is provided by onLoad plugin
-    build.onResolve({ filter: /^\.+\// }, (args: any) => ({
+    build.onResolve({ filter: /^\.+\// }, args => ({
       path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
       namespace: 'a',
     }));
 
     // Resolving module's path (i.e 'lodash', 'react', etc.)
-    build.onResolve({ filter: /.*/ }, (args: any) => ({
+    build.onResolve({ filter: /.*/ }, args => ({
       path: `https://unpkg.com/${args.path}`,
       namespace: 'a',
     }));
