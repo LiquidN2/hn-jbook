7c253a7ee8b72e4ed14447d7b1cd357b267cfdfe
Hugh Nguyen <hhnguyen255@gmail.com>
2022-12-28 17:28:24 +0700

    setup basic md text editor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
-import CodeCell from './components/code-cell';
+// import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App: FC = () => {
   return (
     <div>
-      <CodeCell />
+      {/*<CodeCell />*/}
+      <TextEditor />
     </div>
   );
 };
---
+++ src/components/text-editor.tsx
@@ -0,0 +1,42 @@
+import { FC, useEffect, useRef, useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+const TextEditor: FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('Click to edit your text');
+
+  useEffect(() => {
+    // Disable editing mode if click outside editor
+    const handleClickOutside = (event: MouseEvent) => {
+      // true if click inside editor else false
+      const isClickingInsideEditor =
+        ref.current &&
+        event.target &&
+        ref.current!.contains(event.target as Node);
+
+      setEditing(isClickingInsideEditor as boolean);
+    };
+
+    document.addEventListener('click', handleClickOutside, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  if (editing)
+    return (
+      <div ref={ref} className="md-editor-wrapper">
+        <MDEditor value={value} onChange={(text?: string) => setValue(text!)} />
+      </div>
+    );
+
+  return (
+    <div className="md-editor-wrapper" onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={value} />
+    </div>
+  );
+};
+
+export default TextEditor;
